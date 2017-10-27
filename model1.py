2100a953fa97c29cc315a139c2ebb5f66d3bd767
Ali Rahimi K <ali.rahimikalahroudi@gmail.com>
2017-10-27 16:37:10 +0330

    changes

--- model1.py
+++ model1.py
@@ -6,7 +6,8 @@ from sklearn.utils import shuffle
 
 
 from lasagne import layers
-from lasagne.updates import nesterov_momentum
+from lasagne.updates import adagrad
+#from lasagne.updates import nesterov_momentum
 from nolearn.lasagne import NeuralNet
 
 import matplotlib.pyplot as plt
@@ -17,21 +18,17 @@ from submission import submit
 FTRAIN = 'data/training.zip'
 FTEST = 'data/test.zip'
 
-def load (test=False, cols=None):
+def load (test=False):
     fname = FTEST if test else FTRAIN
-    df = read_csv (fname) # Load pandas dataframe
+    df = read_csv (fname) 
     
     
-    # The Image column has pixel values separated by space; convert
-    # the values to numpy arrays:
     df['Image'] = df['Image'].apply(lambda  im: np.fromstring(im, sep=' '))
-    
-    if cols:
-        df = df[list(cols) + ['Image']]
-        
+
+
     #print (df.count())
     
-    df = df.dropna() # drop all rows that have missing values in the
+    df = df.dropna() 
     
     X = np.vstack(df['Image'].values) / 255 # scale pixel values to [0, 1]
     X = X.astype(np.float32)
@@ -39,8 +36,9 @@ def load (test=False, cols=None):
     if not test: # only FTRAIN has any target columns
         y = df[df.columns[:-1]].values
         y = (y-48) / 48
-        X, y = shuffle(X, y, random_state=42)
         y = y.astype(np.float32)
+
+        X, y = shuffle(X, y, random_state=42)
     else:
         y = None
         
@@ -67,16 +65,15 @@ net1 = NeuralNet(
     
     
     # optimization method
-    update = nesterov_momentum,
+    #update = nesterov_momentum,
+    update = adagrad,
     update_learning_rate = 0.01,
-    update_momentum = 0.9,
+    #update_momentum = 0.9,
     
     
     regression = True, # a flag indicating we're dealing with regression problem
-    max_epochs = 1000, # we want to train this many epochs
+    max_epochs = 50, # we want to train this many epochs
     verbose=1,
-    
-    
     
     )
 
@@ -90,20 +87,20 @@ net1.fit(X, y)
 
 
 
-def learning_curve():
 
-    train_loss = np.array([i["train_loss"] for i in net1.train_history_])
-    valid_loss = np.array([i["valid_loss"] for i in net1.train_history_])
+train_loss = np.array([i["train_loss"] for i in net1.train_history_])
+valid_loss = np.array([i["valid_loss"] for i in net1.train_history_])
 
-    plt.plot(train_loss, linewidth=3, label="train")
-    plt.plot(valid_loss, linewidth=3, label="valid")
-    plt.grid()
-    plt.legend()
-    plt.xlabel("epoch")
-    plt.ylabel("loss")
-    plt.ylim(1e-3, 1e-2)
-    plt.yscale("log")
-    plt.show()
+plt.plot(train_loss, linewidth=3, label="train")
+plt.plot(valid_loss, linewidth=3, label="valid")
+plt.grid()
+plt.legend()
+plt.xlabel("epoch")
+plt.ylabel("loss")
+plt.ylim(1e-5, 1)
+plt.yscale("log")
+plt.show()
+
 
 
 
@@ -134,5 +131,6 @@ def plot_sample_test_data (net) :
 
 
     #np.savetxt("foo.csv", y_pred, delimiter=",")
+
 
 submit(net1, load)
